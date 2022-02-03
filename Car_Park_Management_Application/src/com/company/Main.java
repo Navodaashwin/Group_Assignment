44be3d53e4ac240e01fe3d68eb6109c91556acfa
Navodaashwin <2019t00395@stu.com.ac.lk>
2022-02-03 15:58:50 +0530

    init

--- Car_Park_Management_Application/src/com/company/Main.java
+++ Car_Park_Management_Application/src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //car park manegment application
+        //car park management application
 
     }
 }
